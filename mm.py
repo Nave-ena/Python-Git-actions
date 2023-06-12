241a96d042cf662acd51085e6c52fa6d9b96e2fb
Naveena R <110770588+Nave-ena@users.noreply.github.com>
2023-06-12 14:12:33 +0530

    Update mm.py

--- mm.py
+++ mm.py
@@ -1,1 +1,23 @@
+import smtplib, ssl
+import os
 
+port = 465
+smtp_server = "smtp.gmail.com"
+USER_EMAIL = os.environ.get("USER_EMAIL")
+USER_PASSWORD = os.environ.get("USER_PASSWORD")
+RECEIVER = os.environ.get("RECEIVER")
+
+message = """   
+    Subject: Welcome 
+
+    This is your welcome email running 
+"""
+
+context = ssl.create_default_context()
+
+server = smtplib.SMTP_SSL(smtp_server, port, context=context)
+
+server.login(USER_EMAIL, USER_PASSWORD)
+server.sendmail(RECEIVER,RECEIVER, message)
+          
+   
